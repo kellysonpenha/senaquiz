ac644c34505d9ce3026e88ec6145a6c23997a3ff
danielxs1993 <daniel.xds@hotmail.com>
2016-10-06 21:32:13 -0300

    Tela de perguntas

---
+++ SenaQuiz-GeekQuiz/app/src/main/java/net/sistemasparainter/geekquiz/senaquiz_geekquiz/PerguntasActivity.java
@@ -0,0 +1,13 @@
+package net.sistemasparainter.geekquiz.senaquiz_geekquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerguntasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perguntas);
+    }
+}
